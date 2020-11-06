949334fe128b75685f944dfca2cc1036a622bd6e
Sami Stiti <sami.stiti@nexus2i.com>
2020-11-06 11:12:26 +0100

    Commit Feature 2

--- src/com/mystore/Main.java
+++ src/com/mystore/Main.java
@@ -7,13 +7,7 @@ public class Main
 
    public static void main(String[] args)
    {
-      Store[] store = new Store[1];
-      store[0] = new Store("Founa", "rue de l'or 2030 bhar lazreg");
-      
-      Product product = new Product("P2040", "Eau marwa", new BigDecimal("2.300"), store);
-
-      System.out.println(product);
-      System.out.println(store[0]);
+      System.out.println("feature 2");
    }
 
 }
